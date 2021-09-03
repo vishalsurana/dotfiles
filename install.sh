d47274ce42417c96515d5067b9fe382d9d8b6224
Vishal Surana <vishal.surana@gatech.edu>
2021-09-03 11:22:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ for f in .*; do
 	if [[ -f $f ]]; then ln -s ${HOME}/dotfiles/$f ${HOME}/$f; fi
 done
 rm -rf ${HOME}/.git
+
+git config --global merge.tool vscode
+git config --global mergetool.vscode.cmd 'code --wait $MERGED'
+git config --global diff.tool vscode
+git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE'
